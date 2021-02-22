59220f6da832f5489a6df9cd47bed67ebbbc1041
Matheus Cintra Gonçalves <matheuscg@ymail.com>
2021-02-22 19:50:43 -0300

    docker compose and service discover

---
+++ discover/src/test/java/com/sagaexample/service/discover/DiscoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagaexample.service.discover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
