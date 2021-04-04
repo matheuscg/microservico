75a864598e26c1d22fe46f690352ab3f32aafcdf
Matheus Cintra Gonçalves <matheuscg@ymail.com>
2021-04-04 11:15:41 -0300

    fix compose dependencies and add router healthcheck

---
+++ router/src/main/java/com/netflix/router/Healthcheck.java
@@ -0,0 +1,13 @@
+package com.netflix.router;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Healthcheck {
+    @GetMapping("/healthcheck")
+    public @ResponseBody String healthCheck() {
+        return "OK";
+    }
+}
